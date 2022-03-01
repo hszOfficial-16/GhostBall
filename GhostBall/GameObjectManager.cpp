8a4a47beb8d45ad882c3c802b565ac9d974c3061
hszOfficial-16 <1760432001@qq.com>
2022-03-01 13:38:25 +0800

    底层重构

--- GhostBall/GameObjectManager.cpp
+++ GhostBall/GameObjectManager.cpp
@@ -1,1 +1,31 @@
 #include "GameObjectManager.h"
+
+class GameObjectManager::Impl
+{
+public:
+	size_t nObjectSize;
+	size_t nChunkSize;
+
+public:
+	Impl()
+	{
+		nObjectSize = 0;
+		nChunkSize = 16;
+	}
+	~Impl()
+	{
+
+	}
+};
+
+GameObjectManager::GameObjectManager(size_t nObjectSize, size_t nChunkSize)
+{
+	m_pImpl = new Impl();
+	m_pImpl->nObjectSize = nObjectSize;
+	m_pImpl->nChunkSize = nChunkSize;
+}
+
+GameObjectManager::~GameObjectManager()
+{
+	delete m_pImpl;
+}
--- GhostBall/GameObjectManager.h
+++ GhostBall/GameObjectManager.h
@@ -3,7 +3,24 @@
 
 class GameObjectManager
 {
+public:
 
+
+private:
+	class Impl;
+	Impl* m_pImpl;
+
+public:
+	/* 构造一个对象管理者，负责管理定长对象的生命周期
+	 * @param nObjectSize: 对象大小 
+	 * nChunkSize: 单个区块可容纳对象数量(默认为 16)
+	 */
+	GameObjectManager(
+		size_t nObjectSize,
+		size_t nChunkSize = 16
+	);
+
+	~GameObjectManager();
 };
 
 #endif // !_GAME_OBJECT_MANAGER_H_
