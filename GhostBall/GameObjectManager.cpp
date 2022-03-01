cb0191c5ee2cf50eb4eefbfd8763e70b0e262402
hsz <1760432001@qq.com>
2022-03-01 19:29:37 +0800

    GameObjectManager

--- GhostBall/GameObjectManager.cpp
+++ GhostBall/GameObjectManager.cpp
@@ -1,28 +1,99 @@
 #include "GameObjectManager.h"
 
+#include <vector>
+
 class GameObjectManager::Impl
 {
 public:
-	size_t nObjectSize;
-	size_t nChunkSize;
+	struct MemoryChunk
+	{
+		uintptr_t arrData;
+		MemoryChunk* pNext;
+	};
+	MemoryChunk* m_pChunkHead;
+
+	size_t m_nObjectSize;
+	size_t m_nChunkSize;
+
+	ObjectProcessFunc m_funcConstructor;
+	ObjectProcessFunc m_funcDeconstructor;
+
+	std::vector<void*> m_vFreeBlock;
 
 public:
 	Impl()
 	{
-		nObjectSize = 0;
-		nChunkSize = 16;
+		m_nObjectSize = 0;
+		m_nChunkSize = 16;
+
+		m_funcConstructor = nullptr;
+		m_funcDeconstructor = nullptr;
+
+		m_pChunkHead = nullptr;
 	}
 	~Impl()
 	{
-
+		while (m_pChunkHead)
+		{
+			delete[] (unsigned char*)m_pChunkHead->arrData;
+			delete m_pChunkHead;
+			m_pChunkHead = m_pChunkHead->pNext;
+		}
 	}
 };
+
+void* GameObjectManager::Create()
+{
+	if (m_pImpl->m_vFreeBlock.empty())
+	{
+		Impl::MemoryChunk* pNewChunk = new Impl::MemoryChunk();
+		pNewChunk->arrData = (uintptr_t)new unsigned char[m_pImpl->m_nObjectSize * m_pImpl->m_nChunkSize];
+
+		// 将新区块的空闲内存全部压入栈内
+		for (int i = 0; i < m_pImpl->m_nChunkSize; i++)
+		{
+			m_pImpl->m_vFreeBlock.push_back((void*)(pNewChunk->arrData + i * m_pImpl->m_nObjectSize));
+		}
+
+		if (!m_pImpl->m_pChunkHead)
+		{
+			m_pImpl->m_pChunkHead = pNewChunk;
+		}
+		else
+		{
+			pNewChunk->pNext = m_pImpl->m_pChunkHead;
+			m_pImpl->m_pChunkHead = pNewChunk;
+		}
+	}
+
+	void* pObject = (void*)(*m_pImpl->m_vFreeBlock.end());
+	m_pImpl->m_vFreeBlock.pop_back();
+	m_pImpl->m_funcConstructor(pObject);
+
+	return pObject;
+}
+
+void GameObjectManager::Destroy(void* pObject)
+{
+	m_pImpl->m_funcDeconstructor(pObject);
+	m_pImpl->m_vFreeBlock.push_back(pObject);
+}
+
+void GameObjectManager::SetConstructFunc(ObjectProcessFunc funcConstructor)
+{
+	m_pImpl->m_funcConstructor = funcConstructor;
+}
+
+void GameObjectManager::SetDeconstructFunc(ObjectProcessFunc funcDeconstructor)
+{
+	m_pImpl->m_funcDeconstructor = funcDeconstructor;
+}
 
 GameObjectManager::GameObjectManager(size_t nObjectSize, size_t nChunkSize)
 {
 	m_pImpl = new Impl();
-	m_pImpl->nObjectSize = nObjectSize;
-	m_pImpl->nChunkSize = nChunkSize;
+	m_pImpl->m_nObjectSize = nObjectSize;
+	m_pImpl->m_nChunkSize = nChunkSize;
 }
 
 GameObjectManager::~GameObjectManager()
--- GhostBall/GameObjectManager.h
+++ GhostBall/GameObjectManager.h
@@ -4,7 +4,24 @@
 class GameObjectManager
 {
 public:
+	typedef void(*ObjectProcessFunc)(void*);
 
+	/* 为对象分配内存并调用构造函数
+	* @return 指向该对象的指针
+	*/
+	void* Create();
+
+	/* 调用析构函数并释放对象的内存
+	*/
+	void Destroy(void* pObject);
+
+	/* 设置对象管理者的构造函数
+	*/
+	void SetConstructFunc(ObjectProcessFunc funcConstructor);
+
+	/* 设置对象管理者的析构函数
+	*/
+	void SetDeconstructFunc(ObjectProcessFunc funcDeconstructor);
 
 private:
 	class Impl;
@@ -12,9 +29,9 @@ private:
 
 public:
 	/* 构造一个对象管理者，负责管理定长对象的生命周期
-	 * @param nObjectSize: 对象大小 
-	 * nChunkSize: 单个区块可容纳对象数量(默认为 16)
-	 */
+	* @param nObjectSize: 对象大小 
+	* nChunkSize: 单个区块可容纳对象数量(默认为 16)
+	*/
 	GameObjectManager(
 		size_t nObjectSize,
 		size_t nChunkSize = 16
